8b328ee8327e1c15c1ebb13b28d436d18b24ffb2
Sam Goddard <hello@samgoddard.co.uk>
2021-09-11 17:15:20 +0100

    menu images added

--- schemas/singletonContact.js
+++ schemas/singletonContact.js
@@ -53,6 +53,48 @@ export default {
       validation: Rule => Rule.required()
     },
     {
+      title: "Home Image (Menu)",
+      description: "The image that will appear when hovering the home page on the menu",
+      name: "homeMenuImage",
+      type: 'image',
+      validation: Rule => Rule.required()
+    },
+    {
+      title: "About Us Image (Menu)",
+      description: "The image that will appear when hovering the about page on the menu",
+      name: "aboutMenuImage",
+      type: 'image',
+      validation: Rule => Rule.required()
+    },
+    {
+      title: "Work Image (Menu)",
+      description: "The image that will appear when hovering the work page on the menu",
+      name: "workMenuImage",
+      type: 'image',
+      validation: Rule => Rule.required()
+    },
+    {
+      title: "Clients Image (Menu)",
+      description: "The image that will appear when hovering the clients page on the menu",
+      name: "clientsMenuImage",
+      type: 'image',
+      validation: Rule => Rule.required()
+    },
+    {
+      title: "Digital Showroom Image (Menu)",
+      description: "The image that will appear when hovering the digital showroom page on the menu",
+      name: "digitalShowroomMenuImage",
+      type: 'image',
+      validation: Rule => Rule.required()
+    },
+    {
+      title: "Contact Image (Menu)",
+      description: "The image that will appear when hovering the contact page on the menu",
+      name: "contactMenuImage",
+      type: 'image',
+      validation: Rule => Rule.required()
+    },
+    {
       title: 'SEO / Share Settings',
       name: 'seo',
       type: 'seo'
